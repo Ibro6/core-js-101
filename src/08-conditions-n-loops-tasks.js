a2a0f7c6d68507562dec4e747888dff4f7d7bee8
Ibrokhim <ibrohimechanic@gmail.com>
2020-12-15 00:42:44 +0500

    add solutions

--- src/08-conditions-n-loops-tasks.js
+++ src/08-conditions-n-loops-tasks.js
@@ -176,8 +176,11 @@ function doRectanglesOverlap(/* rect1, rect2 */) {
  *   { center: { x:0, y:0 }, radius:10 },  { x:10, y:10 }   => false
  *
  */
-function isInsideCircle(/* circle, point */) {
-  throw new Error('Not implemented');
+function isInsideCircle(circle, point) {
+  const pointHorizontalOffset = Math.abs(circle.center.x - point.x);
+  const pointVerticalOffset = Math.abs(circle.center.y - point.y);
+
+  return (pointHorizontalOffset ** 2 + pointVerticalOffset ** 2) < circle.radius ** 2;
 }
 
 
